a7b28c45e725e0fe56fa335aac2d19425eb4c288
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-09-12 20:23:00 +0530

    second commit

--- x23.java
+++ x23.java
@@ -1,24 +1,24 @@
-//package com.company;
-import java.util.Scanner;
- class Employee {
-    int id;
-    String name;
-    public void printDetails(){
-        System.out.println("My id is "+id);
-        System.out.println("My name is "+name);
+
+interface Bicycle{
+    int a=45;
+
+    void applyBrake(int decrement);
+     void speedUp(int increment);
+}
+class AvonCycle implements Bicycle{
+    void blowHorn(){
+        System.out.println("pee pee");
 
     }
+    public void applyBrake(int decrement){
+        System.out.println("apply brake");
 
-}
-public class x23 {
+    }
+    public void speedUp(int increment){
+        System.out.println("speed up");
+    }
     public static void main(String[] args) {
-        System.out.println("this is my custom class");
-        Employee shivam=new Employee();
-        shivam.id=1;
-        shivam.name="shivam";
-
-        System.out.println(shivam.id);
-        System.out.println(shivam.name);
-        shivam.printDetails();
+        AvonCycle shivamcycle=new AvonCycle();
+        shivamcycle.applyBrake(1);
     }
 }
