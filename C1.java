42249d3e286a827e71393c3f6ebfa266d24b377d
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-09-26 19:43:48 +0530

    second commit

---
+++ C1.java
@@ -0,0 +1,12 @@
+class C1{
+    public int x=5;
+    public int y=45;
+    public int z=6;
+    private int a=78;
+    public void meth1(){
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+    }
+}
---
+++ x36.java
@@ -0,0 +1,7 @@
+
+public class x36 {
+    public static void main(String[] args) {
+        C1 c=new C1();
+        c.meth1();
+    }
+}
