c9a988f723507bba3bee300a528c3896b08d6ebb
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-11-06 19:57:34 +0530

    first commit

---
+++ src/com/company/x56.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class x56 {
+    public static void main(String[] args) {
+        // HashSet<Integer> myhashset=new HashSet<>();
+        HashSet<Integer> myhashset2=new HashSet<>(2,0.5f);
+        myhashset2.add(6);
+        myhashset2.add(8);
+        myhashset2.add(11);
+        myhashset2.add(11);
+        System.out.println(myhashset2);
+
+    }  
+}
