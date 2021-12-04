90d0a398c7d31a9c18d4767c5034f6b44b694f7d
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-12-04 19:43:18 +0530

    second commit

---
+++ src/com/company/x58.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class x58 {
+    public static void main(String[] args) {
+        // System.out.println(Long.MAX_VALUE);
+        // System.out.println(System.currentTimeMillis());
+        Date d=new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+        System.out.println(d.getYear());
+    }
+    
+}
