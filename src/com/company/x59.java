b94a261e96ca960badf2a2589ba65b14b0a1a1a2
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-12-06 23:50:36 +0530

    second commit

---
+++ src/com/company/x59.java
@@ -0,0 +1,16 @@
+import java.util.Calendar;
+import java.util.*;
+
+// import java.util.TimeZone;
+
+public class x59 {
+
+    public static void main(String[] args) {
+        // Calendar c =Calendar.getInstance();
+        // System.out.println(c.getCalendarType());
+        // System.out.println(c.getTimeZone());\
+        Calendar c =Calendar.getInstance();
+        // System.out.println(c.getCalendarType());
+        System.out.println(c.getTimeZone().getID());
+    }
+}
